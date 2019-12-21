1862670d1746509f08cfdfc8929475a4bf88238c
welcome <sunil18@navgurukul.org>
2019-12-21 21:43:14 +0530

    First API commit

---
+++ src/main/java/com/todos/springboot/web/ApiInJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.todos.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todos/springboot/web/ApiInJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todos.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
